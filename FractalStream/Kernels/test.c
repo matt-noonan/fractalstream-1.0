8da976cd3bd9a6ce2b1fd65bd87186b7066cc838
matt.noonan <matt.noonan@4379a29b-df35-4671-aa0f-b92bac0a062d>
2008-03-28 18:38:59 +0000

    trying to get the right initial version into svn repository
    git-svn-id: svn://localhost/tmp/fractalstream/trunk@3 4379a29b-df35-4671-aa0f-b92bac0a062d

--- FractalStream/Kernels/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	double x[128];
-	int j[3], y;
-	y = 1;
-	x[24] = 1.00000000000000000000e+01;
-	for(j[1] = (int) x[24]; j[1]; j[1]--) { printf("%i\n", y); ++y; }
-}
